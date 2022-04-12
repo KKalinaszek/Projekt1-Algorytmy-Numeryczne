ee364ba5d5ed8bef32bbcde14d81cd50f194fe1c
michalm10 <63353300+michalm10@users.noreply.github.com>
2022-04-12 18:12:02 +0200

    poprawienie małego błędu

--- interpolacja.cpp
+++ interpolacja.cpp
@@ -136,8 +136,11 @@ vector<double> coeff(vector<double>& x, vector<vector<double> >& data) {
                 matrix[i][j] = (matrix[i - 1][j + 1] - matrix[i - 1][j]) / (t[i + j] - t[j]); // bierzemy to co jest po lewej i odejmujemy to co po lewej
             else {                                                                                                // i jeden powy¿ej i dzielimy przez ró¿nicê jak we wzorze
                 int k = j;
-                while (t[k] == t[k - 1]) k--;           // jesli pierwsze i ostatnie t sa rowne to bierzemy wartosc pochodnej z
-                matrix[i][j] = data_list[k + 2 * i - 1] / fact(i);  // wczesniej utworzonej listy  i dzielimy przez silnie
+                while (t[k] == t[k - 1]) k--;
+                if (k == (int)j)            // jesli pierwsze i ostatnie t sa rowne to bierzemy wartosc pochodnej z wczesniej utworzonej listy  i dzielimy przez silnie
+                    matrix[i][j] = data_list[k + i] / fact(i);
+                else
+                    matrix[i][j] = data_list[k + 2 * i - 1] / fact(i);
             }
         }
     }
