ef5cde1ae71225d16e3a117a92ee47e69190a867
michalm10 <63353300+michalm10@users.noreply.github.com>
2022-05-18 17:05:07 +0200

    dodatki

--- projekt2.cpp
+++ projekt2.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -12,36 +14,40 @@ public:
     vector<double> coefficients;
     Polynomian (vector<double> c) {
         int s = c.size();
-        for (int i = 0; c[0] == 0 && i < s; i++)
-            c.erase(c.begin());
+        if (c.size() > 0)
+            for (int i = 0; c[0] == 0 && i < s; i++)
+                c.erase(c.begin());
         coefficients = c;
         degree = coefficients.size() - 1;
     }
+    Polynomian (): degree{0} {}
     double value(double x);
 };
 
 double Polynomian::value(double x) {
     double v = 0;
-    for (int i = 0, j = degree; i < degree + i && j >= 0; i++, j--)
+    for (int i = 0, j = degree; i < degree + 1 && j >= 0; i++, j--)
         v += pow(x, j) * coefficients[i];
     return v;
 }
 
-double bisect(Polynomian p, double as, double bs, double ex, int& iter) {
+double bisect(Polynomian p, double as, double bs, double ex, int& iter, double& error) {
     double a = as, b = bs;
     if (p.value(a) * p.value(b) > 0) return 0;
     double x = (a + b) / 2;
     iter++;
     while (abs(a - x) > ex && iter <= MAX_ITER) {
-        if (p.value(a) * p.value(x) < 0) b = x;
-        else a = x;
-        x = (a + b) / 2;
         iter++;
+        if (p.value(a) * p.value(x) < 0) b = x;
+        else if (p.value(x) * p.value(b) < 0) a = x;
+        else break;
+        x = (a + b) / 2;
     }
+    error = abs(bs - as) / pow(2, iter);
     return x;
 }
 
-double secantMethod(Polynomian p, double a, double b, double ex, int& iter) {
+double secantMethod(Polynomian p, double a, double b, double ex, int& iter, double& error) {
     double x_k1 = a, x_k2 = b;
     double x = x_k1 - (p.value(x_k1) * (x_k1 - x_k2)) / (p.value(x_k1) - p.value(x_k2));
     iter++;
@@ -51,46 +57,83 @@ double secantMethod(Polynomian p, double a, double b, double ex, int& iter) {
         x = x_k1 - (p.value(x_k1) * (x_k1 - x_k2)) / (p.value(x_k1) - p.value(x_k2));
         iter++;
     }
+    error = abs(x_k1 -x);
     return x;
 }
 
 void solve(Polynomian p, double a, double b, double e) {
     int iter = 0;
+    double error;
     if (p.value(a) * p.value(b) < 0) {
         cout << "Metoda polowienia: " << endl;
-        cout << "\tWynik: " << bisect(p, a, b, e, iter) << endl;
+        cout << "\tWynik: " << bisect(p, a, b, e, iter, error) << endl;
         cout << "\tIlosc iteracji: " << iter << endl;
+        cout << "\tPrzyblizony blad: " << error << endl;
     }
     else {
-        cout << "Metoda polowienia wymaga aby wartosci na krancach przedzialow mialy rozne znaki" << endl << endl;
+        cout << "Metoda polowienia wymaga aby wartosci na krancach przedzialow mialy rozne znaki" << endl;
+        if (p.coefficients == vector<double>{1, 0, -22, 3})
+            cout << "Miejsca zerowe sa w przedzialach <-8;-2>; <-2;2>; <2;8>" << endl;
+        cout << endl;
     }
     iter = 0;
     cout << "Metoda siecznych: " << endl;
-    cout << "\tWynik: " << secantMethod(p, a, b, e, iter) << endl;
+    cout << "\tWynik: " << secantMethod(p, a, b, e, iter, error) << endl;
     cout << "\tIlosc iteracji: " << iter << endl;
+    cout << "\tPrzyblizony blad: " << error << endl;
+    cout << "Dokladnosc: " << e << endl;
 }
 
 int main () {
-    Polynomian p = Polynomian({1, 0, -22, 3});
+    Polynomian p0 = Polynomian({1, 0, -22, 3});
     double e, a, b;
     int c;
-    cout << "Podaj krance przedzialu <a, b>: " << endl;
-    cin >> a >> b;
-    if (cin.fail()) {
+    int option;
+    while (true) {
+        Polynomian p;
+        cout << "(1) Wielomian z zadania\n"
+        "(2) Dowolny wielomian\n"
+        "(3) Wyjscie\n";
+        cin >> option;
         cin.clear();
         cin.ignore(256,'\n');
-        cout << "Nieprawidlowa wartosc, podaj jeszcze raz" << endl;
+        switch (option) {
+            case 3: {return 0;}
+            case 2: {
+                cout << "Podaj wspolczynniki(w jednej linii, oddzielone spacjami): " << endl;
+                vector<double> vc;
+                double c;
+                string line;
+                getline(cin, line);
+                istringstream iss(line);
+                while (iss >> c)
+                    if (!iss.fail())
+                        vc.push_back(c);
+                p = Polynomian(vc);
+                break;
+            }
+            case 1: {
+                p = p0;
+            }
+        }
+        cout << "Podaj krance przedzialu <a, b>: " << endl;
         cin >> a >> b;
-    }
-    cout << "Podaj dokladnosc(ilosc miejsc po przecinku): " << endl;
-    cin >> c;
-    if (cin.fail()) {
-        cin.clear();
-        cin.ignore(256,'\n');
-        cout << "Nieprawidlowa wartosc, podaj jeszcze raz" << endl;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(256,'\n');
+            cout << "Nieprawidlowa wartosc, podaj jeszcze raz" << endl;
+            cin >> a >> b;
+        }
+        cout << "Podaj dokladnosc(ilosc miejsc po przecinku): " << endl;
         cin >> c;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(256,'\n');
+            cout << "Nieprawidlowa wartosc, podaj jeszcze raz" << endl;
+            cin >> c;
+        }
+        e = pow(10, -c);
+        solve(p, a, b, e);
     }
-    e = pow(10, -c);
-    solve(p, a, b, e);
     return 0;
 }
